40d75931a157d9118cbc5a4bd689935048fca93a
Superdan56 <71519345+Superdan56@users.noreply.github.com>
2021-09-20 21:08:10 -0400

    game updated

--- Menu/Game Over.gd
+++ Menu/Game Over.gd
@@ -1,7 +1,16 @@
 extends Control
 
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
 func _on_Play_pressed():
    var _scene = get_tree().change_scene("res://Game.tscn")
 
 func _on_Quit_pressed():
   get_tree().quit()
+
+
+
